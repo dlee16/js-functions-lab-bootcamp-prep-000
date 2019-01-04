4b364f644318a5762ba4f2412c6bd5bedb8ae4a0
sensible-shell-8529 <diana.lee116@gmail.com>
2019-01-04 19:11:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,8 +3,14 @@ function happyHolidays(){
   console.log("Happy Holidays!")
 }
 
+function happyHolidaysTo(holiday,name){
+  return `Happy holidays, ${name}!`
+}
+
 function happyHolidayTo(holiday,name){
   return `Happy ${holiday}, ${name}!`
 }
 
-function holidayCountdown(holiday,)
+function holidayCountdown(holiday, days){
+  
+}
